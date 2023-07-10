022358e8ee7c21836f3ad6154acca58982f29f39
alexmb356 <alexmb356@hotmail.com>
2023-07-10 18:39:47 -0300

    limpiando el header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,7 @@
-import {Link} from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+
 
 
 
