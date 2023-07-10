0e432db2aed88cb782fc69a989d3e5196cd9c023
alexmb356 <alexmb356@hotmail.com>
2023-07-10 18:38:28 -0300

    limpiando codigo

--- src/componentes/Peliculas.jsx
+++ src/componentes/Peliculas.jsx
@@ -4,7 +4,7 @@ import { API_URL } from "./API/apiconf";
 import { URL_IMAGE } from "./API/apiconf";
 import {IMAGE_PATH} from "./API/apiconf";
 import YouTube from 'react-youtube';
-import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+
 import axios from "axios";
 
 
@@ -79,59 +79,7 @@ function Peliculas() {
 
 
 
-    /*let pagina = 1;
-  
-    const btnAnterior = (e)=>{
-      if(pagina > 1){
-          pagina -= 1;
-          cargarPeliculas();
-      }
-  }
-  
-      const btnSiguiente = (e)=>{
-          if(pagina < 1000){
-              pagina += 1;
-              cargarPeliculas();
-          }
-    }
-  
-    const cargarPeliculas = async() => {
-  
-      try{
-          const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=191528030c357419329af1198edbcb24&language=es-MX&page=${pagina}`);
-          console.log(respuesta);
-  
-          if(respuesta.status === 200){
-              const datos = await respuesta.json();
-  
-              let peliculas = "";
-              datos.results.forEach(pelicula => {
-                  peliculas += `
-                      <div class="pelicula">
-                          <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" >
-                          <h3 class="titulo"> ${pelicula.title} </h3>
-                          
-                      </div>
-                  
-                  `;
-              }); 
-          
-              document.getElementById("contenedor").innerHTML = peliculas;  
-          }
-  
-          //verificación de mensaje de error para dar al usuario
-          else if(respuesta.status === 401){console.log("key incorrecta");}
-          else if(respuesta.status === 404){console.log("no disponible");}
-          else {console.log("no tengo idea del error")}
-      }
-  
-      catch(error){
-          console.log(error.message);
-          //acciones por si no funciona
-      }
-    }  
-  
-    cargarPeliculas();*/
+
   
     return (
         
@@ -221,23 +169,10 @@ function Peliculas() {
                 </div>
             ))}
             </div>
-           {/* {<div className="row m-5">
-                {movies.map((movie) =>(
-                    <div key={movie.id} className="col-md-3 mb-3">
-                        <img src={`${URL_IMAGE + movie.poster_path}`} alt="" height={600} width="100%"/>
-                        <h4 className="text-center">{movie.title}</h4>
-
-                    </div>
-
-                ))}
-                </div>}*/}
+         
 
         </div>
-          {/*<div className="contenedor bg" id="contenedor"></div>
-          <div className="paginacion">
-              <button onClick={btnAnterior}>Anterior</button>
-              <button onClick={btnSiguiente} id="btnSiguiente">Siguiente</button>
-          </div>*/}
+          
       </div>
     )
   }
